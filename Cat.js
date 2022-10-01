590f2f5e6452a07fa6060ddf740ef547e0eec723
alexanderthinh <alexanderthinh@gmail.com>
2022-10-01 18:28:35 +0700

    Improve cat eat method

--- Cat.js
+++ Cat.js
@@ -2,7 +2,13 @@ function Cat() {
     this.stomach = []
 }
 
-Cat.prototype.eat = (mouse) => {
+Cat.prototype.eat = (animal) => {
+    if(animal == 'mouse') {
+        console.log('eating is ok')
+    } else {
+        console.log('can not eating ' + animal)
+    }
+
     this.stomach.push(mouse)
 }
 
