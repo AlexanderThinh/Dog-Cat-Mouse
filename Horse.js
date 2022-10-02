fa6d598988cc21c564272c05d1e4f98a17449de2
alexanderthinh <alexanderthinh@gmail.com>
2022-10-02 10:13:19 +0700

    Update Horse.js: update jump method 4

--- Horse.js
+++ Horse.js
@@ -2,6 +2,6 @@ function Horse() {
     console.log('I am a horse!!!')
 }
 
-Horse.prototype.jump = function() {
-    console.log('Horse is jumping')
+Horse.prototype.jump4 = function() {
+    console.log('Horse is jumping 4')
 }
